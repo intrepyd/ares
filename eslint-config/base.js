36bceaeeb22d3e36be2fdda151adc98d6c9aba62
Fernando Berti <bertilxi@gmail.com>
2023-04-01 17:23:35 -0300

    chore: clean up deps

--- eslint-config/base.js
+++ eslint-config/base.js
@@ -1,33 +1,42 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
-    createDefaultProgram: false,
-  },
-  plugins: [
-    "@typescript-eslint/eslint-plugin",
-    "simple-import-sort",
-    "sonarjs",
-  ],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:prettier/recommended",
     "plugin:sonarjs/recommended",
     "plugin:unicorn/recommended",
-    "turbo",
+  ],
+  plugins: ["@typescript-eslint", "simple-import-sort", "sonarjs"],
+  parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parserOptions: {
+        project: "tsconfig.json",
+        sourceType: "module",
+        createDefaultProgram: false,
+      },
+    },
   ],
   root: true,
   env: {
     node: true,
   },
-  ignorePatterns: [".eslintrc.js"],
   rules: {
     "no-return-await": "off",
     "@typescript-eslint/return-await": "off",
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
